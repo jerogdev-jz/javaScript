0ab36f07e3cd8763972832de0c955a2ae51c6e04
jerogdev-jz <jerogdev@gmail.com>
2026-09-14 10:13:12 -0600

    Ejercicio 3
    Ejercicio map

--- vanilla-js-ecommerce/js/app.js
+++ vanilla-js-ecommerce/js/app.js
@@ -23,10 +23,31 @@ import products from "./products.js";
 const categoryFiltered = products.filter(producto => 
     producto.category === "home");
 
+// Find()
+
 const productFound = products.find(producto =>
     producto.id === 18);
 
+// Map()
+
+const productTitles = products.map(producto =>
+    producto.title 
+);
+
+const productPrices = products.map(producto =>
+    producto.price 
+);
+
+const productSummary = products.map(producto => ({
+    id: producto.id,
+    title: producto.title,
+    price: producto.price,
+    })
+);
 
 console.log(products);
 console.log(categoryFiltered);
 console.log(productFound);
+console.log(productTitles);
+console.log(productPrices)
+console.log(productSummary);
