ad848457f882aa1acf2797a440f8a8e4a2b02eea
jerogdev-jz <jerogdev@gmail.com>
2026-09-22 17:22:23 -0600

    Ejercicios 9,10, 11 y 12

--- vanilla-js-ecommerce/js/app.js
+++ vanilla-js-ecommerce/js/app.js
@@ -19,21 +19,21 @@ console.log(products);
       return producto.category === "home";
     }); */
     
-// Filter
+// 1 Filter
 
 const categoryFiltered = products.filter(producto => 
     producto.category === "home");
 
 console.log(categoryFiltered);
 
-// Find()
+// 2 Find()
 
 const productFound = products.find(producto =>
     producto.id === 18);
 
 console.log(productFound);
 
-// Map()
+// 3 Map()
 
 const productTitles = products.map(producto =>
     producto.title 
@@ -54,7 +54,7 @@ console.log(productTitles);
 console.log(productPrices)
 console.log(productSummary);
 
-// every() + some()
+// 4 every() + some()
 
 const hasOutOfStock = products.some(producto =>
     producto.stock === 0    
@@ -67,7 +67,7 @@ const allProductsHsaveStock = products.every(producto =>
 console.log(hasOutOfStock);
 console.log(allProductsHsaveStock);
 
-//reduce()
+// 5 reduce()
 
 const totalStock = products.reduce((stock, producto) => {
     return stock + producto.stock;
@@ -80,7 +80,7 @@ const inventoryValue = products.reduce((valorTotalStock, producto) => {
 console.log(totalStock);
 console.log(inventoryValue);
 
-// findindex()
+// 6 findindex()
 
 const productIndex = products.findIndex(producto => 
     producto.id === 15
@@ -88,7 +88,7 @@ const productIndex = products.findIndex(producto =>
 
 console.log(productIndex);
 
-// sort()
+// 8 sort()
 
 const productsByLowestPrice = [...products];
 
@@ -103,3 +103,50 @@ const productsByHighestPrice = [...products];
 
 productsByHighestPrice.sort((a, b) => b.price - a.price);
 console.log(productsByHighestPrice);
+
+// 9 includes()
+
+const searchResults = products.filter(porducto =>
+    porducto.title.includes("LED")
+);
+
+console.log(searchResults);
+
+// 10 includes() + toLowerCase()
+
+const input = "CaFeTeRa"
+
+const searchResultsTwo = products.filter(producto =>
+    producto.title.toLowerCase().includes(input.toLowerCase())
+);
+
+console.log(searchResultsTwo);
+
+// 11 trim()
+
+const inputTwo = "    CaFeTeRa     "
+const normalizedInputTow = inputTwo.trim().toLowerCase()
+
+const searchResultsThree = products.filter(producto =>
+    producto.title.toLowerCase().includes(normalizedInputTow)
+);
+
+console.log(searchResultsThree);
+
+// 12 Filtros convinados
+
+const cat = "Home"
+const word = "Cafetera"
+
+const catN = cat.toLocaleLowerCase().trim()
+const wordN = word.toLocaleLowerCase().trim()
+
+const filteredCatalog = products.filter(producto => 
+        producto.category.toLocaleLowerCase() === catN)
+        .filter(porducto => 
+        porducto.title.toLocaleLowerCase().includes(wordN))
+        .sort((a, b) => a.price - b.price);
+
+
+console.log(filteredCatalog);
+console.log(products);
